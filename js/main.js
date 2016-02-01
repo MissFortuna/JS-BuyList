c5c4d42cc4421077c08f0c6ae572a5966f54ea80
MissFortuna <elizabeth.lemeshko@gmail.com>
2016-02-01 23:21:08 +0200

    Not final, but not bad edition
    Need to add:
    1. Flexible design;
    2. Changing name of product;

--- js/main.js
+++ js/main.js
@@ -2,40 +2,44 @@
  * Created by Elizabeth on 1/24/2016.
  */
 
-
 $(function(){
-//This	function	will	be	called
-//	after	the	DOM	was	loaded	and	ready
-//Here	we	can	place	all	jQuery	Code
-
-//does not work
-
     function createNewElement(title){
         var node = $('<li class="product">'+
-            '<input  class="input-b" value="'+title+'">'+
-            ' <p class="input-bb already-bought">'+title+'</p>'+
-
-            '<div class="box">'+
+            '<input  class="input-b" maxlength=20  value="'+title+'">'+
+            '<span class="box">'+
             '<button class="but down but-block">-</button>'+
             ' <span class="input-c">1</span>'+
             '<button class="but up" ><strong>+</strong></button>'+
-            '/<div>'+
-
+            '</span>'
+            +
             '<button class="buy-button act"><strong>Куплено</strong></button>'+
             '<button class="x-but"><strong>x</strong></button>'+
             '<button class="buy-button-not act"><strong>Не куплено</strong></button>'+
-            '</li>');
 
-        node.find('.buy-button').click(function () {
-            /*$(this).parent().find('.input-b').addClass("crossedOut");*/
+            '</li>'
+        );
+
+        var node2 =$( '<li class=" need-to-buy">'+'<strong>'+title+'</strong>'+
+            '<div class="counter"><strong>1</strong></div>'
+        +'</li>'
+        );
+
+        var node3=$(
+            '<li class="need-to-buy had-to-buy">'+'<strong>'+title+'</strong>'+
+                '<div class="counter booo"><strong>1</strong></div>'+'</li>'
+        );
+
+
+            node.find('.buy-button').click(function () {
+
             node.find('.but').hide();
             node.find('.buy-button').hide();
-            //  $(this).parent().find('.input-c').show();
-            //$(this).className = 'buy-button-not';
-            //node.find('.buy-button').addClass("buy-button-not");
-            //node.find('.buy-button').text("Не куплено");
+            node.find('.buy-button-not').show();
             node.find('.x-but').hide();
-            //$(this).parent().find('.buy-button-not').removeClass("buy-button");
+           node.find('.input-b').attr('disabled',true);
+            node2.hide();
+         node3.show();
+
         });
 
         node.find('.up').click(function(){
@@ -49,69 +53,73 @@ $(function(){
             }
 
             node.find(".input-c").text(old);
+            node2.find(".counter").text(old);
+            node3.find(".counter").text(old);
+
         });
 
-        node.find('.buy-button-not').click(function () {
+        node.find('.down').click(function(){
+            var old=parseInt(node.find(".input-c").text(),10);
 
+            if(old<2){
+                node.find(".down").addClass("but-block");
+                old=1;
+            }
+
+            if((old<=30)&&(old>1)){
+                old--;
+            }
+            node.find(".input-c").text(old);
+            node2.find(".counter").text(old);
+            node3.find(".counter").text(old);
+        });
+
+
+        node.find('.buy-button-not').click(function () {
             node.find('.but').show();
             node.find('.buy-button').show();
-            //node.find('.buy-button-not').addClass("buy-button");
-            //node.find('.buy-button-not').text("Куплено");
+            node.find('.buy-button-not').hide();
             node.find('.x-but').show();
-            //node.find('.buy-button').removeClass("buy-button-not");
+            node.find('.input-b').show();
+            node.find('.input-b').attr('disabled',false);
+            node2.show();
+            node3.hide();
         });
 
         node.find(".x-but").click(function () {
             node.remove();
+            node2.remove();
+            node3.remove();
         });
+
+        /*
+        node.find('.input-b').focus(function () {
+            var before =node.find('.input-b').val();
+            node2.find(".need-to-buy").text("before");
+        });
+*/
+
         $("ul.edition").append(node);
+        $("ul.still_need").append(node2);
+        $("ul.webuyit").append(node3);
     }
+
+
 
     $('button.bluebutton').click(function(){
         var input =$("#main-input");
         var newElem = input.val();
         if(newElem!==""){
             input.val("");
+            input.focus();
             createNewElement(newElem);
+
         }
     });
-    // it is work, but color of block button start work from 2.
-    $('.up').click(function(){
-        var old=$(this).parent().find(".input-c").text();
-
-        if(old>=2){
-            $(this).parent().find(".down").removeClass("but-block");
-        }
-        var num=1;
-        for(var i=1;i<30;i++){
-            if(i==old){
-                num=i+1;
-                break;
-            }
-        }
-        $(this).parent().find(".input-c").text(num);
-    });
-    $('.down').click(function(){
-        var old=$(this).parent().find(".input-c").text();
-        if(old==1){
-            $(this).parent().find(".down").addClass("but-block");
-        }
-        if(old>=2){
-            $(this).parent().find(".down").removeClass("but-block");
-        }
-        var num=1;
-        for(var i=30;i>1;i--){
-            if(i==old){
-                num=i-1;
-                break;
-            }
-        }
-
-        $(this).parent().find(".input-c").text(num);
-    });
-
 
     createNewElement("Смородинка");
-    createNewElement("Смородинка");
-    createNewElement("Смородинка");
+    createNewElement("Полуничка");
+    createNewElement("Шоколадка");
+
+
 });
